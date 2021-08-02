f5af11172c2f5e0dcca5939bed6092b7d05f8534
hdkim <eastshine94@naver.com>
2021-08-02 21:58:16 +0900

    recoil 맛보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import {
+  atom,
+  RecoilState,
+  selector,
+  useRecoilState,
+  useRecoilValue
+} from 'recoil';
 
+const textState: RecoilState<string> = atom({
+  key: 'textState',
+  default: ''
+});
+const charCountState = selector({
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  }
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <TextInput />
+      <CharacterCount />
     </div>
   );
 }
 
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <div>Character Count: {count}</div>;
+}
 export default App;
